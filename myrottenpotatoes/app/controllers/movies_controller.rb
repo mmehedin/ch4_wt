5a46bf26a20ed25018a2690572b6e0b5f0cd609e
Mihai Mehedint <mmehedin@uncc.edu>
2016-03-07 19:22:47 +0000

    fixed movies variable in controller

--- myrottenpotatoes/app/controllers/movies_controller.rb
+++ myrottenpotatoes/app/controllers/movies_controller.rb
@@ -1,6 +1,8 @@
 class MoviesController < ActionController::Base
 
-
+  def index
+    @movies = Movie.all
+  end
 
 def show
   id = params[:id] # retrieve movie ID from URI route
@@ -12,6 +14,7 @@ end
 def new
   # default: render 'new' template
 end
+
 #def create
 #  @movie = Movie.create!(params[:movie])
 #  redirect_to movies_path
@@ -28,29 +31,28 @@ end
 
 # in movies_controller.rb
 
-def edit
-  @movie = Movie.find params[:id]
-end
+  def edit
+    @movie = Movie.find params[:id]
+  end
 
-def update
-  @movie = Movie.find params[:id]
-  #@movie.update_attributes!(params[:movie])
-  @movie.update_attributes!(movie_params)
-  flash[:notice] = "#{@movie.title} was successfully updated."
-  redirect_to movie_path(@movie)
-end
+  def update
+    @movie = Movie.find params[:id]
+    #@movie.update_attributes!(params[:movie])
+    @movie.update_attributes!(movie_params)
+    flash[:notice] = "#{@movie.title} was successfully updated."
+    redirect_to movie_path(@movie)
+  end
 
-def destroy
-  @movie = Movie.find(params[:id])
-  @movie.destroy
-  flash[:notice] = "Movie '#{@movie.title}' deleted."
-  redirect_to movies_path
-end
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    flash[:notice] = "Movie '#{@movie.title}' deleted."
+    redirect_to movies_path
+  end
 
-private
-def movie_params
-      params.require(:movie).permit(:title, :rating, :description, :release_date)
-end
-
+  #private
+  def movie_params
+        params.require(:movie).permit(:title, :rating, :description, :release_date)
+  end
 
 end
